c6c50d673847774174e5fcaa4eb6225d1e31cee3
searchme86 <searchme86@gmail.com>
2022-11-30 11:21:32 +0900

    🐛  fix path bug for firebase-config

--- src/lib/Firebase.ts
+++ src/lib/Firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { FirebaseConfig } from '../../Firebase.config';
+import { FirebaseConfig } from './Firebase.config';
 
 const firebaseConfig = FirebaseConfig;
 
