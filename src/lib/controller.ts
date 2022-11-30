658d3e6eae30971f1efc49a8039131ae132619d2
searchme86 <searchme86@gmail.com>
2022-11-30 16:40:25 +0900

    ✨ add Feature of Edit Document in Firebase9

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -4,9 +4,11 @@ import {
   collection,
   deleteDoc,
   getFirestore,
+  setDoc,
 } from 'firebase/firestore';
 import { app } from './Firebase';
 import { AddHotelType } from '../Types/hotels';
+import { NavigateFunction } from 'react-router-dom';
 
 // firebase에 지정한 데이터베이스, hotels에 접근하기 위해
 // 글로벌하게 접근하도록 셋팅함
@@ -25,9 +27,20 @@ export const addHotel = async (hotelData: AddHotelType) => {
 };
 
 //Delete a document in your collection
-export const deleteHotel = async (id: string | undefined, navigate: any) => {
+export const deleteHotel = async (
+  id: string | undefined,
+  navigate: NavigateFunction
+) => {
   const document = doc(firestore, `hotels/${id}`);
   await deleteDoc(document);
   console.log('hotel is deleted');
   navigate('/');
+};
+
+// Edit a Document
+export const updateHotel = async (id: string | undefined, docData: any) => {
+  // 현재 해당하는 도큐먼트의 id(호텔하나의 데이터를 참조하는 값)
+  const getHotel = doc(firestore, `hotels/${id}`);
+  await setDoc(getHotel, docData, { merge: true });
+  console.log('the value has been written');
 };
